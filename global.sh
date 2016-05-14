3996bf611aabbc38217692b7fe6007134bccd2af
Super-Baleine <paul.feuvraux@protonmail.ch>
2016-05-14 16:55:54 +0200

    feature added

--- global.sh
+++ global.sh
@@ -6,11 +6,12 @@ if [[ "$EUID" -ne 0 ]]; then
 	exit 1
 fi
 #----------- CHOICE -----------
-while [[ $choice != "1" || $choice != "2" ]]; do
+while [[ $choice != "1" || $choice != "2" || $choice != "3" ]]; do
 	read -p "Welcome !
 	Make a choice between :
 	1) create the NAT router
-	2) delete the NAT router" choice
+	2) delete the NAT router
+	3) reconfigure IP" choice
 done
 
 case $choice in
@@ -43,6 +44,29 @@ case $choice in
 			sysctl -p
 			rm /etc/iptables_rules.save
 			echo 'Finished !'
+			;;
+		"3")
+			read -p "Enter your public ip : " $public_ip
+			read -p "Enter the netmask of the eth0 (public) : " $public_netmask
+			read -p "Enter the router's IP : " $routing
+			read -p "Enter your private ip : " $private_ip
+			read -p "Enter the netmask of eth1 (private) : " $private_netmask
+			echo "auto eth0
+			iface eth0 inet static
+			address $public_ip
+			netmask $public_netmask
+			gateway $routing ##Ip privée du routeur physique
+			post-up iptables-restore < /etc/iptables_rules.save
+
+			auto eth1
+			iface eth1 inet static
+			address $private_ip
+			netmask $private_netmask" > /etc/network/interfaces;
+			systemctl restart networking || /etc/init.d/networking restart
+			echo "Finished !
+			Router's IP : $routing
+			Your public IP is : $public_ip with netmask $public_netmask
+			Your private IP is : $private_ip with netmask $private_netmask";
 			;;
 esac
 echo "
